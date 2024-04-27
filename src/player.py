715a726e129db2485c75d390282392571911f4bd
Adyarel <138512882+Adyarel@users.noreply.github.com>
2024-04-27 18:29:34 +0200

    :bug: block boat when game frezzing

--- src/player.py
+++ src/player.py
@@ -11,3 +11,11 @@ class Player(Boat):
 
     def __init__(self, screen: pygame.Surface, map_data: Map, pos: Pos, speed: Speed, mass, max_power, boat_image):
         super().__init__(screen, map_data, pos, speed, mass, max_power, boat_image)
+
+    def run(self, time_step, camera_pos: Pos):
+        if time_step < 0.2:
+            super().run(time_step, camera_pos)
+
+    def rotate(self, angle, time_step, map_data: Map):
+        if time_step < 0.2:
+            super().rotate(angle, time_step, map_data)
